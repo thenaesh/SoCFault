f84fa392f7e098bffc6d9d0a97e22d89f462de9a
Thenaesh Elango <thenaeshelango@gmail.com>
2017-09-22 02:18:40 +0800

    Cosmetic updates to Config.hs

--- src/Config.hs
+++ src/Config.hs
@@ -7,29 +7,38 @@ module Config
     ) where
 
 import qualified Data.ByteString.Lazy as B
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Data.Maybe
 import Data.Text
 import Data.Aeson
 import GHC.Generics
 
 
-loadConfig :: (FromJSON t) => FilePath -> IO (Maybe t)
-loadConfig fileName = B.readFile path >>= (return . decode)
+loadConfig :: FilePath -> MaybeT IO Config
+loadConfig fileName = MaybeT $ do
+    file <- B.readFile path
+    let config = decode file
+    when (isNothing config) $ putStrLn errorMsg
+    return config
     where
         path = mconcat [basePath, "/", fileName]
         basePath = "config"
+        errorMsg = "Error reading " ++ fileName ++ "!"
 
 data Config = Config
-    { botToken :: !Text
-    , dbConfig :: !DBConfig
+    { _botToken :: !Text
+    , _dbConfig :: !DBConfig
     } deriving (Show, Generic)
 
 instance FromJSON Config
 
 data DBConfig = DBConfig
-    { host :: !Text
-    , db :: !Text
-    , user :: !Text
-    , password :: !Text
+    { _host :: !Text
+    , _db :: !Text
+    , _user :: !Text
+    , _password :: !Text
     } deriving (Show, Generic)
 
 instance FromJSON DBConfig
